0b927ece9113700c66220c786ee794c8c0e50c72
Romeo V. Seva III <100087338+RJSeebs02@users.noreply.github.com>
2025-02-05 17:42:37 +0800

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
+
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '$50,000'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary' => '$10,000'
+    ],
+    [
+        'id' => 3,
+        'title' => 'Teacher',
+        'salary' => '$40,000'
+    ],
+];
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/jobs', function () use ($jobs) {
+    return view('jobs', [
+        'jobs'=> $jobs
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) use ($jobs){
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
